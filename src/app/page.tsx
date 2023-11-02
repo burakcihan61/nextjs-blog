3cf0edbb6a910ac6fa362eaf978a4a687333e246
Burak Cihan <burakcihan622@gmail.com>
2023-11-02 15:37:02 +0300

    feat: components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,12 @@ import Link from "next/link"
 
 import { siteConfig } from "@/config/site"
 import { buttonVariants } from "@/components/ui/button"
+import Featured from "@/components/featured"
 
 export default function IndexPage() {
   return (
     <div>
-      <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-        Beautifully designed components <br className="hidden sm:inline" />
-        built with Radix UI and Tailwind CSS.
-      </h1>
+      <Featured />
     </div>
   )
 }
